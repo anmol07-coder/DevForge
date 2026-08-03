b6c033b4791b554d61676c3f7e052dff3c21933b
Anmol Tyagi <tyagiji2005@gmail.com>
2026-08-03 10:44:25 +0530

    add user data model

---
+++ src/models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        lowercase : true,
+        unique : true,
+        trim : true
+    },
+    password : {
+        type : String,
+        required : true,
+        select : false,
+    },
+    isEmailVerified : {
+        type : Boolean,
+        default : false
+    },
+    role : {
+        type : String,
+        enum: ["member", "admin"],
+        default: "member"
+    }
+},
+{
+    timestamps : true
+});
+
+const User = mongoose.model("User" , userSchema);
+
+module.exports = User;
